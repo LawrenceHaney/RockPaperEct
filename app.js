ba74e343d3a8db0694efb0a457e52e1704eab4b5
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-08-19 13:23:55 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// data
+let data: [
+  {
+    name: "Rock",
+    loss: "Paper"
+  }
+  {
+    name: "Paper"
+    loss: "Scissor"
+  }
+  {
+    name: "Scissor"
+    loss: "Rock"
+  }
+]
+
+// Game
+thowhand(data){
+//findNPCelement
+  //acesse data
+  //set NPC arry to RND choice using MATH.Random
+//findPlayerelemnt.loss
+  //compareplayer element.loss to NPC choice
+    //if loss "I win"
+    //if win "how about another hand?"
+}
+
+//Draw
+drawcard(data)
+template= ''
+data.forEach(element => {
+});
+
